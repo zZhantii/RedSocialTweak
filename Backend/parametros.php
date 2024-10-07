7cf29c94e72e0a0debeb96e19ded48b0726ed0ca
Santiago <santiagolozadafp23@ibf.cat>
2024-10-07 11:54:40 +0200

    RedSocial

---
+++ Backend/parametros.php
@@ -0,0 +1,5 @@
+<?php
+    //Defino una URL Base para coger la Matriz para acortar las rutas.
+    define("url_base","http://localhost/dawredsocial/");
+    define("dir_img","C:/xampp/htdocs/dawredsocial/Frontend/images/");
+?>
